f68a749abe03e3a6db4f80ed295f4d48a7294477
Aravind Sairam <asairam@lucidbots.com>
2024-10-04 21:37:18 +0200

    added ros_localization launch and param files

---
+++ isaac_ros_occupancy_grid_localizer/launch/local_ekf.launch.py
@@ -0,0 +1,20 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration
+import os
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    robot_localization_node = Node(
+        package='robot_localization',
+        executable='ekf_node',
+        name='ekf_filter_node',
+        output='screen',
+        parameters=[os.path.join(
+            get_package_share_directory('isaac_ros_occupancy_grid_localizer'),'params', 'local_ekf.yaml')],
+    )
+
+
+    return LaunchDescription([robot_localization_node])
